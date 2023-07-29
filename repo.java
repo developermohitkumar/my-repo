828a263d7abc054a334ac9f6d29de55f0a543423
Mohit <mtm7037@gmail.com>
2023-07-29 10:58:31 +0530

    commited 2

--- repo.java
+++ repo.java
@@ -1,5 +1,6 @@
 class repo{
 public static void main(String args[]){
-System.out.print("hii what is your name");
+System.out.println("hii what is your name");
+System.out.println("hii what is your age");
 }
 }
