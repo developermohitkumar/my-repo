dff9db09aa7ce537b18b262ef605c578cd3e99a6
Mohit <mtm7037@gmail.com>
2023-07-29 11:48:49 +0530

    branch1 commited

--- repo.java
+++ repo.java
@@ -2,5 +2,6 @@ class repo{
 public static void main(String args[]){
 System.out.println("hii what is your name");
 System.out.println("hii what is your age");
+System.out.println("this is the branch 1");
 }
 }
