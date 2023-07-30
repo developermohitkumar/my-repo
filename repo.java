0a2f69865113cca5247ef37dff4c9cd374cfcda9
Mohit <mtm7037@gmail.com>
2023-07-30 09:06:10 +0530

    version update

--- repo.java
+++ repo.java
@@ -3,13 +3,11 @@ public static void main(String args[]){
 System.out.println("hii what is your name");
 System.out.println("hii what is your age");
 
-<<<<<<< HEAD
-System.out.println("this is the branch 2");
 
 
-=======
-System.out.println("this is branch1");
->>>>>>> branch1
+ 
+
+
 
 }
 }
