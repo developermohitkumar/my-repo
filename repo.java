2875d55ae08021ccf26e2aa3cb1c56c0cfc229ee
Mohit <mtm7037@gmail.com>
2023-07-30 09:50:33 +0530

    time pass

--- repo.java
+++ repo.java
@@ -2,18 +2,6 @@ class repo{
 public static void main(String args[]){
 System.out.println("hii what is your name");
 System.out.println("hii what is your age");
-<<<<<<< HEAD
 time pass
-=======
-
-<<<<<<< HEAD
-System.out.println("this is the branch 2");
->>>>>>> parent of 0a2f698 (version update)
-
-
-=======
-System.out.println("this is branch1");
->>>>>>> branch1
-
 }
 }
