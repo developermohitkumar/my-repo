696741c8943c41aa4c3ba9a3571b41dfa15734ce
Mohit <mtm7037@gmail.com>
2023-07-30 09:51:45 +0530

    amend command

--- repo.java
+++ repo.java
@@ -3,5 +3,14 @@ public static void main(String args[]){
 System.out.println("hii what is your name");
 System.out.println("hii what is your age");
 time pass
+<<<<<<< HEAD
+=======
+
+
+ 
+
+
+
+>>>>>>> parent of 692cfdf (revert)
 }
 }
